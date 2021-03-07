62cbc78cfc02775adfdfc50004b2ce261a5822a1
Mena Wahba <menawahba@gmail.com>
2021-03-06 22:51:53 -0500

    uploading to heroku

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# noteTaker
+# **Note Taker**
+
+
+
+* [Description](description)
+
+* [Installation](installation)
+
+* [Information](information)
+
+
+![](public/assets/noteTaker.JPG)
+
+
+## Description
+An app that allows the user to take notes and save them. It is an easy app to use, The user Enters a title and the information that is needed then save. The information that is saved can be retrieved or deleted as needed.
+## Installation
+Download the program and in the terminal enter "node server" to open the port. then in the browser enter localhost:8080 to use the app. 
+## Information
+When the app is running in the browser, enter titles and text to save, delete if not needed.
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = 8080;
+const PORT = 8080;
 const saveNotes = path.join(__dirname, "/public");
 
 app.use(express.static('public'));
@@ -26,8 +26,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(saveNotes, "index.html"));
 });
 app.post("/api/notes", (req, res) => {
-
-    //   * GET`/api/notes` - Should read the`db.json` file and return all saved notes as JSON.
+//   * GET`/api/notes` - Should read the`db.json` file and return all saved notes as JSON.
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let note = req.body;
     let ID = (savedNotes.length).toString();
@@ -39,23 +38,23 @@ app.post("/api/notes", (req, res) => {
 })
 //   * DELETE`/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.
 app.delete("/api/notes/:id", (req, res) => {
-    //   In order to delete a note, you'll need to read all notes from the `db.json` file, 
+//   In order to delete a note, you'll need to read all notes from the `db.json` file, 
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let ID = req.params.id;
     let incID = 0;
     savedNotes = savedNotes.filter(currNote => {
         return currNote.id != ID;
     })
-    //   This means you'll need to find a way to give each note a unique `id` when it's saved.
+//   This means you'll need to find a way to give each note a unique `id` when it's saved.
     for (currNote of savedNotes) {
         currNote.id = incID.toString();
         incID++;
     }
-    //   remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+//   remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
     fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
     res.json(savedNotes);
 })
-
-app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
+// PORT listner
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
 })
